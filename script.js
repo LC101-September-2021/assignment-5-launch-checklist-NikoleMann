d862056fb11a1ccf9cbd01136295e712faf62739
NikoleMann <Nikole.d.mann@gmail.com>
2021-12-07 18:58:37 -0600

    Got the alerts working!!!

--- script.js
+++ script.js
@@ -1,16 +1,57 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+// const { validateInput, formSubmission } = require("./scriptHelper");
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
+window.addEventListener("load", function() {
+    //this script adds an event when you hit the button "Submit", this event should:
+    //1)Check that all boxes have info added to them
+    //2}Not accept the default values using preventDefault()
+    //3)Ensure the input is the correct Value type.
+    //4) !!!! uses the formSubmission() function !!!
+    //5) if the criteria are not met it stops the page from moving on 
+    //6) if they are met it updates the faultyItems list (this is done inside the formSubmission() function I think)
+    
+    const pilot = document.querySelector("input[name=pilotName]");
+    const copilot = document.querySelector("input[name=copilotName");
+    const fuelLevel = document.querySelector("input[name=fuelLevel]");
+    const cargoLevel = document.querySelector("input[name=cargoMass]");
+
+    let form = document.querySelector("form");
+    document.addEventListener("submit", function(event) {
+        event.preventDefault()
+        formSubmission(pilot, copilot, fuelLevel, cargoLevel)
+    })
+        
+        
+
+
+        //formSubmission updates the li items if everything passes
+        //next if the cargo mass or fuel level are outside acceptable parameters they should be made visible with a message that they are
+        //not at acceptable levels. 
+        //in this case shuttle status should be red and read "Shuttle not ready for launch"
+        //if everything passes launch status should turn to green and read "Shuttle is ready for launch"    
+    
+    
+    
+    
+    
+
+    
+
+
+
+
+
+
+//    let listedPlanets;
+//    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+//    let listedPlanetsResponse;
+//    listedPlanetsResponse.then(function (result) {
+//        listedPlanets = result;
+//        console.log(listedPlanets);
+//    }).then(function () {
+//        console.log(listedPlanets);
+//        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+//    })
    
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,5 +1,5 @@
 // Write your helper functions here!
-require('isomorphic-fetch');
+// require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
@@ -17,11 +17,34 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+    console.log(testInput.value);
+    console.log(isNaN(testInput.value));
+    if (testInput.value === "") {
+        console.log("undefined")
+        return "undefined"
+    }
+    else if (isNaN(testInput.value)) {
+        console.log("testInput is working")
+        return "Not a number"
+    } else {
+        return "Is a number"
+    }
 }
 
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+function formSubmission(pilot, copilot, fuelLevel, cargoLevel) {
+    
+    if (validateInput(pilot) === "undefined" || validateInput(copilot) === "undefined" || validateInput(fuelLevel) === "undefined" || validateInput(cargoLevel) === "undefined") {
+        alert("All fields must be filled out.")  
+    }
+    else if (validateInput(pilot) === "Is a number" || validateInput(copilot) === "Is a number") {
+        alert("Make sure to enter valid information for each field.")
+    } 
+    else if (validateInput(fuelLevel) === "Not a number" || validateInput(cargoLevel) === "Not a number") {
+        alert("Make sure to enter valid information for each field.")
+    } else {
+        // make the status for each of the fields updated to what was put in. 
+        // this does nothing right now but if I take it out the function stops
+    }
 }
 
 async function myFetch() {
